1b750d240060350be2c3c572f867df9d6afc07c4
Daksh Kaushik <dakshkaushik20040126@gmail.com>
2024-03-23 04:18:39 +0530

    "adding the project"

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+fn main() {
+    
+
+    let config = Config::new(env::args()).unwrap_or_else(|err|{
+        eprintln!("Problem parsing arguments: {}",err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}",config.query);
+    println!("in file {}",config.file_name);
+
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application Error: {}",e);
+        process::exit(1);
+    };
+}
+
